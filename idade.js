39ff82931a9bd27602f100be15ff63b667a7dd56
Hudson L. Paixão <134105562+Hudlp@users.noreply.github.com>
2024-05-01 12:48:08 -0300

    changes

--- idade.js
+++ idade.js
@@ -2,10 +2,10 @@
 
 var botao = document.querySelector('input[value="Confirmar"]')
 botao.addEventListener("click", (event) => {
-  votacao(event)
+  checkUserAgeAndSetMessage(event)
 })
 
-function votacao(e) {
+function checkUserAgeAndSetMessage(e) {
   e.preventDefault()
   var rIdade = document.querySelector("input#txtidade")
   var resIdade = document.querySelector("div#resultado")
@@ -16,19 +16,24 @@ function votacao(e) {
     resIdade.innerHTML = "<strong>Digite sua idade</strong>"
     resIdade.style.color = "red"
     rIdade.style.border = "solid 1px red"
-  } else {
-    if (idade < 16) {
-      resIdade.innerHTML = "<strong>Não precisa votar</strong>"
-      resIdade.style.color = "#37447c"
-      rIdade.style.border = "solid 1px #37447c"
-    } else if (idade < 18 || idade >= 65) {
-      resIdade.innerHTML = "<strong>Voto opcional</strong>"
-      resIdade.style.color = "#37447c"
-      rIdade.style.border = "solid 1px #37447c"
-    } else {
-      resIdade.innerHTML = "<strong>Voto obrigatório</strong>"
-      resIdade.style.color = "#37447c"
-      rIdade.style.border = "solid 1px #37447c"
-    }
+    return
   }
+
+  if (idade < 16) {
+    resIdade.innerHTML = "<strong>Não precisa votar</strong>"
+    resIdade.style.color = "#37447c"
+    rIdade.style.border = "solid 1px #37447c"
+    return
+  }
+
+  if (idade < 18 || idade >= 65) {
+    resIdade.innerHTML = "<strong>Voto opcional</strong>"
+    resIdade.style.color = "#37447c"
+    rIdade.style.border = "solid 1px #37447c"
+    return
+  }
+
+  resIdade.innerHTML = "<strong>Voto obrigatório</strong>"
+  resIdade.style.color = "#37447c"
+  rIdade.style.border = "solid 1px #37447c"
 }
